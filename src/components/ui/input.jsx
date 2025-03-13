aa6db61955af7bdbbbd97a9ae19842e1eb04dbbf
Ana Clara Alleva <aclaraalleva@gmail.com>
2025-03-13 15:46:38 -0300

    Add favicon, remove inputs add shadows

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -12,12 +12,12 @@ const Input = React.forwardRef(({ className, type, iconLeft, ...props }, ref) =>
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-gray-300 bg-white py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          iconLeft ? "pl-10 pr-3" : "px-3",
-          className
+          "flex h-9 w-full rounded-md border border-gray-300 bg-white py-1 text-base shadow-sm transition - colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm shadow overflow-hidden hover:border-purple-500 hover:shadow-[0px_1px_2px_0px_rgba(16,24,40,0.1),_0px_0px_0px_4px_rgba(158,119,237,0.4)] focus:shadow-[0px_1px_2px_0px_rgba(16,24,40,0.1),_0px_0px_0px_4px_rgba(158,119,237,0.5)] transition-shadow duration-300",
+      iconLeft ? "pl-10 pr-3" : "px-3",
+      className
         )}
-        ref={ref}
-        {...props}
+      ref={ref}
+      {...props}
       />
     </div>
   );
